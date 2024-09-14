df58d1bd8452b831c94a02ac7e12ef8811475eb6
João Dematte <demattejoao@gmail.com>
2024-09-14 19:50:07 -0300

    initial commit w react compiler and react 19

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/**
+ * {@type require('prettier').Config}
+ */
+module.exports = {
+  plugins: [],
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSameLine: false,
+  semi: true,
+  quoteProps: 'consistent'
+};
